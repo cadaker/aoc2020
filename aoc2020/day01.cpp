dcb9ac95eaa73f85c9048bde7196bda43ee37077
Christian Adåker <cadaker@gmail.com>
2020-12-01 08:26:29 +0100

    Day 01, part 2.

--- aoc2020/day01.cpp
+++ aoc2020/day01.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <tuple>
 
 std::pair<long, long> find_pair_summing_to(std::vector<long> const& input, long target) {
     for (size_t i = 0; i < input.size(); ++i) {
@@ -11,6 +12,19 @@ std::pair<long, long> find_pair_summing_to(std::vector<long> const& input, long 
         }
     }
     return {-1, -1};
+}
+
+std::tuple<long, long, long> find_triplet_summing_to(std::vector<long> const& input, long target) {
+    for (size_t i = 0; i < input.size(); ++i) {
+        for (size_t j = i+1; j < input.size(); ++j) {
+            for (size_t k = j+1; k < input.size(); ++k) {
+                if (input[i] + input[j] + input[k] == target) {
+                    return {input[i], input[j], input[k]};
+                }
+            }
+        }
+    }
+    return {-1, -1, -1};
 }
 
 int main() {
@@ -24,4 +38,7 @@ int main() {
 
     auto part1 = find_pair_summing_to(input, 2020);
     std::cout << part1.first * part1.second << "\n";
+
+    auto part2 = find_triplet_summing_to(input, 2020);
+    std::cout << get<0>(part2) * get<1>(part2) * get<2>(part2) << "\n";
 }
