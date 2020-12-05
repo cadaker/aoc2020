425e710c53286f441f85c8fdfae80fce14ad9d6a
Christian Adåker <cadaker@gmail.com>
2020-12-05 22:51:06 +0100

    Range for reading line-by-line from an input stream.

---
+++ aoc2020/input_views.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <ranges>
+#include <memory>
+
+class input_line_iterator: public std::iterator<
+        std::input_iterator_tag,
+        std::string,
+        std::ptrdiff_t,
+        std::string const*,
+        std::string const&
+        > {
+public:
+    input_line_iterator()
+            : _stream(nullptr)
+    {}
+
+    explicit input_line_iterator(std::istream& is)
+            : _stream(&is)
+    {
+        read();
+    }
+
+    input_line_iterator(input_line_iterator const&) = default;
+    input_line_iterator(input_line_iterator&&) = default;
+    input_line_iterator& operator=(input_line_iterator const&) = default;
+    input_line_iterator& operator=(input_line_iterator&&) = default;
+
+    friend bool operator==(input_line_iterator const& x, input_line_iterator const& y) {
+        return x._stream == y._stream;
+    }
+
+    friend bool operator!=(input_line_iterator const& x, input_line_iterator const& y) {
+        return !(x == y);
+    }
+
+    reference operator*() const {
+        return _line;
+    };
+
+    pointer operator->() {
+        return &_line;
+    }
+
+    input_line_iterator& operator++() {
+        read();
+        return *this;
+    }
+
+    input_line_iterator operator++(int) {
+        input_line_iterator ret = *this;
+        read();
+        return ret;
+    }
+
+private:
+    void read() {
+        if (_stream && !std::getline(*_stream, _line)) {
+            _stream = nullptr;
+        }
+    }
+
+    std::istream* _stream;
+    std::string _line;
+};
+
+static_assert(std::input_iterator<input_line_iterator>);
+
+
+class input_lines {
+public:
+    explicit input_lines(std::istream& is)
+            : _stream(is)
+    {}
+
+    using iterator = input_line_iterator;
+
+    [[nodiscard]] iterator begin() const {
+        return iterator(_stream);
+    }
+
+    [[nodiscard]] iterator end() const {
+        return iterator();
+    }
+
+private:
+    std::istream& _stream;
+};
+
+static_assert(std::ranges::input_range<input_lines>);
+
+template<>
+constexpr bool std::ranges::enable_borrowed_range<input_lines> = true;
---
+++ tests/input_views.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "input_views.hpp"
+
+#include <sstream>
+#include <vector>
+
+TEST(input_views, input_lines_range) {
+    std::istringstream iss(R"(foo
+bar
+baz
+qux quux quuux)");
+    input_lines line_range(iss);
+    std::vector lines(line_range.begin(), line_range.end());
+
+    ASSERT_EQ(lines.size(), 4);
+    EXPECT_EQ(lines[0], "foo");
+    EXPECT_EQ(lines[1], "bar");
+    EXPECT_EQ(lines[2], "baz");
+    EXPECT_EQ(lines[3], "qux quux quuux");
+}
