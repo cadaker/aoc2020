1c488bb8ea813411d6b783a52363d182d384c9ee
Christian Adåker <cadaker@gmail.com>
2020-12-17 00:49:46 +0100

    Day 16, part 2

--- aoc2020/day16.cpp
+++ aoc2020/day16.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <regex>
 #include <sstream>
@@ -107,12 +108,78 @@ int validate_ticket(ticket_t const& ticket, constraints_t const& constraints) {
     return invalid_sum;
 }
 
+bool is_valid(ticket_t const& ticket, constraints_t const& constraints) {
+    for (int field : ticket) {
+        if (!is_any_valid(field, constraints)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int ticket_scanning_error_rate(std::vector<ticket_t> const& tickets, constraints_t const& constraints) {
     return tickets | std::views::transform([&](ticket_t const& t) { return validate_ticket(t, constraints); }) | accumulate(0);
+}
+
+bool validate_column(ticket_t const& ticket, size_t column, constraint_t const& constraint) {
+    return is_valid(ticket.at(column), constraint);
+}
+
+using possible_labels_t = std::vector<std::unordered_set<std::string>>;
+
+possible_labels_t label_tickets(std::vector<ticket_t> const& tickets, constraints_t const& constraints) {
+    std::vector<ticket_t> const valid_tickets =
+            tickets
+            | std::views::filter([&](auto& t) { return is_valid(t, constraints); })
+            | to_vector;
+
+    if (valid_tickets.empty()) {
+        return {};
+    }
+
+    possible_labels_t labeling;
+    for (size_t column = 0; column < tickets.front().size(); ++column) {
+        std::unordered_set<std::string> labels;
+        for (auto& [name, constraint] : constraints) {
+            if (std::ranges::all_of(valid_tickets, [&](ticket_t const& t) { return validate_column(t, column, constraint); })) {
+                labels.insert(name);
+            }
+        }
+        labeling.push_back(std::move(labels));
+    }
+    return labeling;
+}
+
+std::vector<std::string> reduce_labeling(possible_labels_t possible_labels) {
+    std::vector<std::string> reduced_labels(possible_labels.size());
+    while (true) {
+        auto it = std::ranges::find_if(possible_labels, [](auto const& labels) { return labels.size() == 1; });
+        if (it == possible_labels.end()) {
+            return reduced_labels;
+        } else {
+            std::string const label = *it->begin();
+            reduced_labels.at(std::distance(possible_labels.begin(), it)) = label;
+            for (std::unordered_set<std::string>& set : possible_labels) {
+                set.erase(label);
+            }
+        }
+    }
 }
 
 void run() {
     problem_t const problem = parse(std::cin);
 
     std::cout << ticket_scanning_error_rate(problem.nearby_tickets, problem.constraints) << std::endl;
+
+    auto const possible_labels = label_tickets(problem.nearby_tickets, problem.constraints);
+    auto const labeling = reduce_labeling(possible_labels);
+
+    long product = 1;
+    for (size_t column = 0; column < labeling.size(); ++column) {
+        if (labeling[column].starts_with("departure")) {
+            product *= problem.your_ticket.at(column);
+        }
+    }
+    std::cout << product << std::endl;
+
 }
